13f7031fd31864688c095681b559aaf599e8b128
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2023-07-10 15:36:13 +0300

    Add project files.

---
+++ Ecom.BFF/Program.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Dependency Injections
+
+
+#endregion
+
+#region Database - Identity Configurations
+//builder.Services.AddDbContext<EcomContext>(options =>
+//  options.UseSqlServer(builder.Configuration.GetConnectionString("EcomContext")));
+
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+//{
+//    options.Password.RequiredLength = 4;
+//    options.Password.RequireNonAlphanumeric = false;
+//    options.Password.RequireDigit = false;
+//    options.Password.RequireUppercase = false;
+//})
+//    .AddEntityFrameworkStores<EcomContext>()
+//    .AddDefaultTokenProviders();
+
+//builder.Services.ConfigureApplicationCookie(options =>
+//{
+//    options.Cookie.Name = "ChatiusCokie";
+//});
+
+#endregion
+
+#region Services
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder.AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .SetIsOriginAllowed((host) => true)
+                   .AllowCredentials();
+        }));
+var app = builder.Build();
+
+#endregion
+
+#region Middlewares Pipeline
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapDefaultControllerRoute();
+//    endpoints.MapHub<ChatHub>("/hub", options =>
+//    {
+//        options.Transports =
+//               HttpTransportType.WebSockets |
+//               HttpTransportType.LongPolling;
+//    });
+
+//});
+
+app.MapDefaultControllerRoute();
+
+try
+{
+    app.Run();
+}
+catch (Exception)
+{
+    throw;
+}
+#endregion
---
+++ Ecom.SPA/Startup.cs
@@ -0,0 +1,42 @@
+﻿using VueCliMiddleware;
+
+namespace Iwcp.Web.SPA.Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var isTestingEnv = env.IsEnvironment("Test");
+
+            if (env.IsDevelopment() || isTestingEnv)
+                app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                if (env.IsDevelopment() || isTestingEnv)
+                    spa.Options.SourcePath = "ClientApp/dist";
+                else
+                    spa.Options.SourcePath = "dist";
+
+                if (env.IsDevelopment())
+                    spa.UseVueCli(npmScript: "serve", port: 8000, false, ScriptRunnerType.Npm, "ready in", true);
+                else if (isTestingEnv)
+                    spa.UseVueCli(npmScript: "test", port: 7000, false, ScriptRunnerType.Npm, "ready in", true);
+            });
+        }
+    }
+}
